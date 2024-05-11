04fa913f7150724847ed6cb94fd07d5d22affcf5
arya-syamudra <aryasyamudra24@gmail.com>
2024-05-12 00:15:40 +0800

    Insight Baru

---
+++ js/index.js
@@ -0,0 +1,84 @@
+// header 
+const menuIcon = document.querySelector(".menu-icon");
+const navMenu = document.querySelector(".nav-menu");
+
+menuIcon.addEventListener("click", () => {
+    menuIcon.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    menuIcon.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
+
+
+//Insight
+document.addEventListener("DOMContentLoaded", function() {
+    const insights = document.querySelectorAll('.insight-div');
+  
+    insights.forEach(function(insight) {
+      const question = insight.querySelector('.question');
+  
+      question.addEventListener('click', function() {
+        insight.classList.toggle('active');
+      });
+    });
+  });
+
+
+function generateInsight(question, answers) {
+
+    const insightItem = document.createElement('div');
+    insightItem.classList.add('insight-div');
+
+    const questionElement = document.createElement('h1');
+    questionElement.classList.add('question');
+    questionElement.textContent = question;
+  
+    insightItem.appendChild(questionElement);
+
+    const answerContainer = document.createElement('div');
+    answerContainer.classList.add('answer');
+
+    answers.forEach((answer) => {
+      const answerItem = document.createElement('div');
+      answerItem.classList.add('answer-item');
+      answerItem.textContent = answer;
+
+      answerContainer.appendChild(answerItem);
+    });
+  
+    insightItem.appendChild(answerContainer);
+  
+    return insightItem;
+  }
+  
+  const insightData = [
+    {
+      question: 'Daftar Pawang Hujan',
+      answer: [
+        'Mbak Rara',
+        'Kobokan Aeru'
+      ]
+    },
+    {
+      question: 'Waga na wa Megumin! Ākuwizādo wo nariwai toshi, saikyou no kougeki no mahou "bakuretsu mahou" wo ayatsuru mono!',
+      answer: [
+        'ORA!.!.!. ORA!.!.! ORA!.!.!. ORA!.!.!. ORA!.!.!. ORA!.!.!. ORA!.!.!. ORA!.!.!. ORA!.!.!. ORA!.!.!. ORA!.!.!. ORA!.!.!. ORA!.!.!. ORA!.!.!. ORA!.!.!. ',
+        'MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!. MUDA.!.!.',
+        'MCLaren Lu Warna Apa Bos?'
+      ]}
+  ];
+
+  const insightListContainer = document.querySelector('.insight-list');
+
+  insightData.forEach((insightItem) => {
+    const insightElement = generateInsight(insightItem.question, insightItem.answer);
+    if (insightListContainer) {
+      insightListContainer.appendChild(insightElement);
+    } else {
+      document.body.appendChild(insightElement);
+    }
+  });
+  
